973497087a20900b9066f52e86f0cef4108053ee
cobodo <cobodo@gmail.com>
2017-12-25 02:33:28 +0900

    twitter-text 2系と1系の両対応

--- mikutter_justsize.rb
+++ mikutter_justsize.rb
@@ -1,9 +1,17 @@
 Plugin.create :justsize do
   class Gtk::PostBox < Gtk::EventBox
+    def extractor
+      if Twitter.const_defined?(:Extractor)
+        Twitter::Extractor
+      else
+        Twitter::TwitterText::Extractor
+      end
+    end
+
     def remain_charcount
       if not widget_post.destroyed?
         text = trim_hidden_regions(widget_post.buffer.text + UserConfig[:footer])
-        Twitter::Extractor.extract_urls(text).map{|url|
+        extractor.extract_urls(text).map{|url|
           if url.length < posted_url_length(url)
             -(posted_url_length(url) - url.length)
           else
